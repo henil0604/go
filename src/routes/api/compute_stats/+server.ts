962a46723e1da3407d73d4fd957f0338f79a3e95
Henil Malaviya <henilmalaviya06@gmail.com>
2023-09-06 18:09:26 +0530

    optimized compute_stats api response time by making sure that more redirects happened before computing new stats

--- src/routes/api/compute_stats/+server.ts
+++ src/routes/api/compute_stats/+server.ts
@@ -1,8 +1,21 @@
 import { prisma } from "$lib/server/db";
 import computeLinkStats from "$lib/server/utils/computeLinkStats";
 import type { RequestHandler } from "./$types";
+import { kv } from "@vercel/kv";
 
 export const GET: RequestHandler = async (event) => {
+
+    const KV_PREVIOUS_REDIRECT_COUNTS_KEY = '__go_api_compute_stats_previous_redirects_count'
+
+    const kvPreviousRedirectCounts = await kv.get(KV_PREVIOUS_REDIRECT_COUNTS_KEY);
+
+    const current_redirects = await prisma.redirect.findMany();
+
+    if (kvPreviousRedirectCounts && current_redirects.length === kvPreviousRedirectCounts) {
+        return new Response('SAME');
+    } else {
+        await kv.set(KV_PREVIOUS_REDIRECT_COUNTS_KEY, current_redirects.length);
+    }
 
     const links = await prisma.link.findMany();
 
