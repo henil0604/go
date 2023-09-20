08a47df22ce4596a93ef24552fdfcc85b176ba70
Henil Malaviya <henilmalaviya06@gmail.com>
2023-09-20 10:27:11 +0530

    feat: instant redirect if all checks passed at server

--- src/routes/[id]/+page.server.ts
+++ src/routes/[id]/+page.server.ts
@@ -2,7 +2,7 @@ import getLinkByAlias from "$lib/server/utils/getLinkByAlias";
 import getLinkById from "$lib/server/utils/getLinkById";
 import { redirect } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
-
+import { trpc } from "$lib/trpc/client";
 
 export const load: PageServerLoad = async (event) => {
     const { params } = event;
@@ -13,6 +13,17 @@ export const load: PageServerLoad = async (event) => {
     const link = await getLinkById(id) || await getLinkByAlias(id);
 
     console.log("link?", link);
+
+    // if link does not require password and link also does not require any authentication, just redirect the user from the server
+    if (link && link?.passwordProtection === false && link.requiresAuth === false) {
+        const decryptedUrl = await trpc(event).decryptURL.query(link.url);
+
+        await trpc(event).registerRedirect.query({
+            linkId: link.id
+        });
+
+        throw redirect(301, decryptedUrl);
+    }
 
     return {
         link
